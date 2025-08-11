57555e0cffa2f51b680d280eca8a5e1b051de9a9
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-08-11 21:17:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import LogginForm from "./LogginForm";
 
 const App = () => {
+   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-        <h1>Parent Component</h1>
+      <h1>Parent Component</h1>
       {!isLoggedIn ? (
         <LogginForm setIsLoggedIn={setIsLoggedIn} />
       ) : (
         <p>You are logged in!</p>
       )}
-    </div>
+   </div>
   );
 }
 
